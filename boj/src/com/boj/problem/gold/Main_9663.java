119d362f8cd9966c5a90a867578127d45e4344c7
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-02-02 04:01:39 +0900

    N-Queen clear(더 좋은 답이 있을듯)

--- boj/src/com/boj/problem/gold/Main_9663.java
+++ boj/src/com/boj/problem/gold/Main_9663.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
 public class Main_9663 {
 	static int N;
 	static int[][] board;
-	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
 	static ArrayList<Integer> rDiagonal;
-	static ArrayList<Integer> lDiagonal;
+	static ArrayList<Integer> cantCol;
 	static int count;
 	
 	public static void main(String[] args) {
@@ -20,10 +18,10 @@ public class Main_9663 {
 		/* 초기화 */
 		board = new int[N][N];
 		rDiagonal = new ArrayList<>();
-		lDiagonal = new ArrayList<>();
+		cantCol = new ArrayList<>();
 		
 		/* 로직 */
-		queen(0, 1);
+		queen(0, 0);
 
 		
 		/* 출력 */
@@ -32,19 +30,39 @@ public class Main_9663 {
 
 	private static void queen(int r, int q) {
 		//퀸을 모두 놓았을 때
-		if(q == N) return;
-		
-		if(!rDiagonal.contains(Math.abs(r-c)) && !lDiagonal.contains(r+c)) {
-			board[r][c] = 1; //퀸 배치
-			rDiagonal.add(Math.abs(r-c));
-			lDiagonal.add(r+c);
+		if(q == N) {
+			count++;
+			return;
 		}
 		
 		for (int c = 0; c < N; c++) {
 			if(isValid(r, c)) {
-				
+				q++; //퀸 배치
+				board[r][c] =1;
+				cantCol.add(c);
+				rDiagonal.add(r+c);
+				queen(r+1, q); //다음 퀸 배치
+				q--;
+				board[r][c] = 0;
+				cantCol.remove(cantCol.size()-1);
+				rDiagonal.remove(rDiagonal.size()-1);
 			}
 		}
 		
+	}
+
+	private static boolean isValid(int r, int c) {
+		if(!cantCol.contains(c) && !rDiagonal.contains(r+c)) {
+			for (int i = 0; i < r; i++) {
+				for (int j = 0; j < N; j++) {
+					if(board[i][j] == 1) {
+						if(c == j) return false;
+						if(Math.abs(r-i) == Math.abs(c-j)) return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
 	}
 }
