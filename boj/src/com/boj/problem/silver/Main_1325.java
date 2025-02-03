e394ab2da96c530e43de0067b7ddfceab8b1d931
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-02-03 23:40:52 +0900

    효율적 해킹 clear

--- boj/src/com/boj/problem/silver/Main_1325.java
+++ boj/src/com/boj/problem/silver/Main_1325.java
@@ -3,11 +3,17 @@ package com.boj.problem.silver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_1325 {
 	static int N, M;
-	static ArrayList<ArrayList<Integer>> B;
+	static ArrayList<Integer>[] com;
+	static boolean[] visited;
+	static int max;
+	static int[] maxIdx;
 
 	public static void main(String[] args) throws Exception{
 		/* 입력 */
@@ -17,34 +23,50 @@ public class Main_1325 {
 		M = Integer.parseInt(st.nextToken());
 		
 		/* 초기화 */
-		B = new ArrayList<ArrayList<Integer>>();
-		for (int i = 0; i < N+1; i++) {
-			B.add(new ArrayList<Integer>());
+		com = new ArrayList[N+1]; //타입은 안정하지 않지만 코테에선 쓸만할 듯?
+		for (int i = 0; i <= N; i++) {
+			com[i] = new ArrayList<>();
 		}
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			B.get(a).add(b);
+			com[a].add(b);
 		}
+		visited = new boolean[N+1];
+		maxIdx = new int[N+1];
 		
 		/* 로직 */
-		int max = 0;
-		ArrayList<Integer> result = null;
-		for (ArrayList<Integer> a : B) {
-			max = Math.max(max, a.size());
-			result = a;
+		for (int i = 1; i <= N; i++) {
+			if(com[i].isEmpty()) continue;
+			//bfs
+			Queue<Integer> q = new LinkedList<>();
+			q.offer(i);
+			visited[i] = true;
+			
+			while(!q.isEmpty()) {
+				int cur = q.poll();
+				for (int j = 0; j < com[cur].size(); j++) {
+					int ni = com[cur].get(j);
+					if(visited[ni]) continue;
+					q.offer(ni);
+					visited[ni] = true;
+					maxIdx[ni]++;
+				}
+			}
+			
+			visited = new boolean[N+1];
 		}
 		
-		하는 중
-		N과M이 1일경우를 생각해야할 듯?
+		//최댓값만 추려내기 (index순이라 순서는 자동임)
+		for (int d : maxIdx) {
+			if(d != 0) max = Math.max(max, d);
+		}
 		
 		/* 출력 */
 		StringBuilder sb = new StringBuilder();
-		if(result != null) {
-			for (int i = 0; i < result.size(); i++) {
-				sb.append(result.get(i) + " ");
-			}
+		for (int i = 0; i <= N; i++) {
+			if(maxIdx[i] == max) sb.append(i + " ");
 		}
 		System.out.println(sb.toString());
 	}
