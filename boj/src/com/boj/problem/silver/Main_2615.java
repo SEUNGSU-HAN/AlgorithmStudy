f3afc047b10248937e8ecac7f5d31c3ed7867e0d
SSAFY <SSAFY@70.12.107.126>
2025-02-05 17:52:41 +0900

    25.02.05 현장

---
+++ boj/src/com/boj/problem/silver/Main_2615.java
@@ -0,0 +1,30 @@
+package com.boj.problem.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2615 {
+	static int N = 19;
+	static int[][] board;
+	static int[] dr
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		/* 초기화 */
+		board = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		/* 로직 */
+		
+		하는 중
+	}
+
+}
