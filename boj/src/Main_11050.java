e32a99fbecfc72974a0133137abe83864b3ce57d
juhyeon <dlwngus3856@naver.com>
2025-02-21 12:00:29 +0900

    이항 계수1 clear

---
+++ boj/src/Main_11050.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_11050 {
+	static int N, K;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		
+		System.out.print(factorial(N)/(factorial(N-K)*factorial(K)));
+	}
+
+	private static int factorial(int n) {
+		if(n <= 1) return 1;
+		return n*factorial(n-1);
+	}
+
+}
