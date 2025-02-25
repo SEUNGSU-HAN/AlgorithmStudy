7c93faee5a85be7310f4a5050bdb5468f64d540a
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-02-26 00:00:39 +0900

    야미 팰린드롬수 clear (문자열)

---
+++ boj/src/Main_1259.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+//팰린드롬수
+public class Main_1259 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String str = "";
+		while(!(str = br.readLine()).equals("0")) {
+			boolean result = true;			
+			char[] cl = str.toCharArray();
+			int s = 0, e = cl.length-1;
+			while(s < e) {
+				if(cl[s++] != cl[e--]) {
+					result = false;
+				}
+			}
+			sb.append(result ? "yes" : "no").append("\n");
+		}
+		System.out.print(sb);
+	}
+
+}
