18ec8cf5d6d5cf67574868a0928e853585ae32bc
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-03-02 03:08:17 +0900

    아기 상어 푸는 중

---
+++ boj/src/Main_17086.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main_17086 {
+	static int N, M;
+	static int[][] board;
+	static ArrayList<int[]> shark;
+	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+	static boolean[][] visited;
+	static int maxD;
+	
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		/* 초기화 */
+		board = new int[N][M];
+		shark = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if(board[i][j] == 1) shark.add(new int[] {i, j});
+			}
+		}
+		
+		/* 로직 */
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(board[i][j] == 0) {
+					visited = new boolean[N][M];
+					bfs(new int[] {i, j, 0});
+				}
+			}
+		}
+		
+		/* 출력 */
+		System.out.println(maxD);
+	}
+
+	static void bfs(int[] start) {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(start);
+		visited[start[0]][start[1]] = true;
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int nr = cur[0]+dr[i];
+				int nc = cur[1]+dc[i];
+				if(check(nr, nc)) {
+					
+				}
+			}
+		}
+	}
+
+	static boolean check(int nr, int nc) {
+		return (0 <= nr && nr < N) && (0 <= nc && nc < M);
+	}
+
+}
