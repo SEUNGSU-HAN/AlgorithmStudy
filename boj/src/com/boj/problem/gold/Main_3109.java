5fbcb7c79a51f5736237daf41dc35bc657cfbbf4
KKaeBu <h2sorginal@gmail.com>
2025-03-02 20:15:06 +0900

    빵집 푸는 중...

--- boj/src/com/boj/problem/gold/Main_3109.java
+++ boj/src/com/boj/problem/gold/Main_3109.java
@@ -2,12 +2,14 @@ package com.boj.problem.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-틀림... 뭐가 틀렸을까..?
 public class Main_3109 {
 	static int R, C;
 	static char[][] board;
+	static boolean[][] visited;
 	static int[] dr = {-1, 0, 1};
 	static int pipe;
 
@@ -20,35 +22,18 @@ public class Main_3109 {
 		
 		//초기화
 		board = new char[R][C];
+		visited = new boolean[R][C];
 		for (int i = 0; i < R; i++) {
-			String line = br.readLine();
+			board[i] = br.readLine().toCharArray();
 			for (int j = 0; j < C; j++) {
-				board[i] = line.toCharArray();
+				if(board[i][j] == 'x') visited[i][j] = true;
 			}
 		}
-		int r = 0;
-		int c = 0;
-		int count = 0;
+		
 		
 		//로직
 		for (int i = 0; i < R; i++) {
-			r = i;
-			c = 0;
-			count = 0;
-			for (int j = 0; j < C; j++) {
-				for (int k = 0; k < 3; k++) {
-					int nr = r+dr[k];
-					int nc = c+1;
-					if((nr >= 0 && nr < R) && (nc < C) && board[nr][nc] == '.') {
-						board[nr][nc] = 'a';
-						r = nr;
-						c = nc;
-						count = 0;
-						break;
-					}else count++;
-				}
-				if(count == 3) break;
-			}
+			dfs(0, new int[] {i, 0});
 		}
 		
 		for (int i = 0; i < R; i++) {
@@ -56,13 +41,23 @@ public class Main_3109 {
 		}
 		
 		//출력
-//		StringBuilder sb = new StringBuilder();
-//		for (int i = 0; i < R; i++) {
-//			sb.append(board[i]).append("\n");
-//		}
-//		System.out.println(sb.toString());
 		System.out.println(pipe);
 		
+	}
+
+	static void dfs(int cnt, int[] cur) {
+		if(cnt == C) {
+			return;
+		}
+		for (int i = 0; i < 3; i++) {
+			int nr = cur[0]+dr[i];
+			int nc = cur[1]+1;
+			if(check(nr, nc) && board[nr][nc] == '.' )
+		}
+	}
+
+	static boolean check(int nr, int nc) {
+		return (0 <= nr && nr < R) && (0 <= nc && nc < C);
 	}
 
 }
