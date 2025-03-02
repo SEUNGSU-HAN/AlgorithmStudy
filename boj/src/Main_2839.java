6d11661bc018afb862751e8e69a2edfb56e7811e
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-03-03 03:08:28 +0900

    설탕 배달 하는 중..

---
+++ boj/src/Main_2839.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main_2839 {
+	static int N, result;
+
+	public static void main(String[] args) {
+		/* 입력 */
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		/* 초기화 */
+		
+		/* 로직 */
+		하는 중
+		
+		/* 출력 */
+	}
+
+}
