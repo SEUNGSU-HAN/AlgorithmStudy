d6cccf387da95410cac3e4b7aef9337c77309deb
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-03-04 00:04:52 +0900

    설탕 배달 clear (그리디)

--- boj/src/Main_2839.java
+++ boj/src/Main_2839.java
@@ -1,19 +1,31 @@
 import java.util.*;
 
 public class Main_2839 {
-	static int N, result;
+	static int N, result = -1;
 
 	public static void main(String[] args) {
 		/* 입력 */
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		
-		/* 초기화 */
-		
 		/* 로직 */
-		하는 중
+		//최대 5000이 들어와도 3으로 나눈 값이 1666
+		//그래서 최악이라도 대충 1700*1700=2,890,000 언저리 (널널함)
+		//그리디 느낌으로 
+		int m = (int)Math.ceil(N/3);
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= m; j++) {
+				int n = 5*i+3*j;
+				if(n == N) {
+					if(result == -1) result = i+j;
+					else result = Math.min(result, i+j);
+				}
+			}
+		}
 		
 		/* 출력 */
+		System.out.print(result);
 	}
+
 
 }
