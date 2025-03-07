23690b4e412a0bb78d9b6806ea9764e1ebe24622
SSAFY <SSAFY@host.docker.internal>
2025-03-07 17:50:18 +0900

    집합 푸는 중...

---
+++ boj/src/Main_11723.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_11723 {
+	static int M, n;
+	static String str;
+	static Set<Integer> set;
+
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		
+		/* 초기화 */
+		
+		/* 로직 */
+		
+		/* 출력 */
+		
+	}
+
+}
