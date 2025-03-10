665efe6cad696bd7b5ae28a6ff1756eecc279c75
SSAFY <SSAFY@host.docker.internal>
2025-03-10 17:53:18 +0900

    젤다 최적화 중

--- boj/src/test.java
+++ boj/src/test.java
@@ -1,28 +1,13 @@
-package com.boj.problem.gold;
 import java.io.*;
 import java.util.*;
 
-public class Main_4485 {
+public class test {
 	static final int INF = 999;
 	static int N, test_case = 1;
 	static int[][] board;
-	static int[][] dp;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
-	static class Node implements Comparable<Node>{
-		int r, c, w;
-
-		public Node(int r, int c, int w) {
-			this.r = r;
-			this.c = c;
-			this.w = w;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
+	static int[] dp;
 
 	public static void main(String[] args) throws Exception{
 		/* 입력 */
@@ -40,38 +25,33 @@ public class Main_4485 {
 					board[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			dp = new int[N][N];
-			for(int i = 0; i < N; i++) {
-				Arrays.fill(dp[i], INF);
-			}
+			dp = new int[N*N];
+			Arrays.fill(dp, INF);
 			
 			/* 로직 */
 			//1. 다익스트라
-			
-			//개선 ver
-			PriorityQueue<Node> q = new PriorityQueue<>();
-			Node start = new Node(0, 0, board[0][0]);
+			PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+			int[] start = {0, 0, board[0][0]};
 			q.offer(start);
-			dp[0][0] = board[0][0];
+			dp[0] = board[0][0];
 			while(!q.isEmpty()) {
-				Node cur = q.poll();
-				System.out.println("cur0: " + cur.r + ", cur1: " + cur.c);
-				if(cur.r == N-1 && cur.c == N-1) break;
-				
+				int[] cur = q.poll();
+				System.out.println("cur0: " + cur[0] + ", cur1: " + cur[1]);
+                if(cur[0] == N-1 && cur[1] == N-1) break;
 				for (int i = 0; i < 4; i++) {
-					int nr = cur.r+dr[i];
-					int nc = cur.c+dc[i];
+					int nr = cur[0]+dr[i];
+					int nc = cur[1]+dc[i];
 					if(check(nr, nc)) {
-						if(dp[nr][nc] > dp[cur.r][cur.c]+board[nr][nc]){
-							dp[nr][nc] = dp[cur.r][cur.c]+board[nr][nc];
-							q.offer(new Node(nr, nc, dp[nr][nc]));
+						int pre = cur[0]*N+cur[1]%N;
+						int next = nr*N+nc%N;
+						if(dp[next] > dp[pre] + board[nr][nc]) {
+							dp[next] = dp[pre] + board[nr][nc];
+							q.offer(new int[] {nr, nc, board[nr][nc]});
 						}
 					}
 				}
 			}
-			
-			sb.append(String.format("Problem %d: %d\n", test_case++, dp[N-1][N-1]));
-			
+			sb.append("Problem ").append(test_case++).append(": ").append(dp[N*N-1]).append("\n");
 			N = Integer.parseInt(br.readLine().trim());
 		}
 		/* 출력 */
