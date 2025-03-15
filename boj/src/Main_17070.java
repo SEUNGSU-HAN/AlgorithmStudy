bdb75875cd06916cee4536a96685b96c6a0efcbf
KKaeBu <h2sorginal@gmail.com>
2025-03-15 16:26:53 +0900

    파이프 옮기기1 clear (bfs)

--- boj/src/Main_17070.java
+++ boj/src/Main_17070.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class Main_17070 {
 	static int N, set=0; //set: 현대 파이프가 놓인 상태(0:우,1:하,2:대)
+	static int[][][] d = {
+			{{0, 1}, {0, 0}, {1, 1}}, //우로 놓였을 때
+			{{0, 0}, {1, 0}, {1, 1}}, //하로 놓였을 때
+			{{0, 1}, {1, 0}, {1, 1}} //대각선으로 놓였을 때
+	};
 	static int[][] board;
 
 	public static void main(String[] args) throws Exception{
@@ -20,17 +25,44 @@ public class Main_17070 {
 			}
 		}
 		
-		/* 로직 */
-		bfs(new int[] {0, 1});
-		
-		/* 출력 */
-		
+		/* 로직+출력 */
+		System.out.print(bfs(new int[] {0, 1, 0})); //r, c, 파이프 상태		
 	}
 
-	static void bfs(int[] start) {
+	static int bfs(int[] start) {
 		Queue<int[]> q = new ArrayDeque<>();
 		q.offer(start);
+		int count = 0;
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int s = cur[2];
+			if(cur[0] == N-1 && cur[1] == N-1) count++;
+			for (int i = 0; i < d[s].length; i++) {
+				int nr = cur[0]+d[s][i][0];
+				int nc = cur[1]+d[s][i][1];
+				if(nr == cur[0] && nc == cur[1]) continue;
+				if(check(nr, nc) && board[nr][nc] != 1) {
+					if(i == 2) //대각선으로 갈 경우 현재 위치에서 우,하,대 체크
+						if(!check2(cur[0], cur[1])) continue;
+					q.offer(new int[] {nr, nc, i});
+				}
+			}
+		}
 		
+		return count;
+	}
+
+	static boolean check2(int r, int c) {
+		for (int i = 0; i < d[2].length; i++) {
+			int nr = r+d[2][i][0];
+			int nc = c+d[2][i][1];
+			if(board[nr][nc] == 1) return false;
+		}
+		return true;
+	}
+
+	static boolean check(int nr, int nc) {
+		return (0 <= nr && nr < N) && (0 <= nc && nc < N);
 	}
 
 }
