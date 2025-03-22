b19ffccc22e57d9f8aea0c12dec0fc8cfe4fbe34
KKaeBu <h2sorginal@gmail.com>
2025-03-22 19:03:33 +0900

    막대기 clear(재귀, 비트마스킹, 라이브러리활용 3가지 방법으로 풀 수 있음)

--- boj/src/com/boj/problem/silver/Main_1094.java
+++ boj/src/com/boj/problem/silver/Main_1094.java
@@ -16,27 +16,27 @@ public class Main_1094 {
 		
 		/* 로직 + 출력 */
 		//비트마스킹 풀이
-//		int idx = 0;
-//		int count = 0;
-//		while((1<<idx) <= X) if((X & 1<<idx++) != 0) count++;
-//		System.out.print(count);
+		int idx = 0;
+		int count = 0;
+		while((1<<idx) <= X) if((X & 1<<idx++) != 0) count++;
+		System.out.print(count);
 		
 		
 		/* 로직 + 출력 */
 		//라이브러리 활용 풀이
-		System.out.print(Integer.bitCount(X));
+//		System.out.print(Integer.bitCount(X));
 		
 	}
 
-	static int findStick(int s, int x) {
-		if(s == x) return 1;
-		
-		if(x == 0) return 0;
-
-		int half = s/2;
-		if(x >= half)
-			return findStick(half, x-half) + 1;
-		else return findStick(half, x);
-	}
+//	static int findStick(int s, int x) {
+//		if(s == x) return 1;
+//		
+//		if(x == 0) return 0;
+//
+//		int half = s/2;
+//		if(x >= half)
+//			return findStick(half, x-half) + 1;
+//		else return findStick(half, x);
+//	}
 
 }
