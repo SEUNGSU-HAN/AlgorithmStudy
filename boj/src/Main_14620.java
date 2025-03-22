29a0d11c324e2fe5f154bcfe7698345418a121cf
KKaeBu <h2sorginal@gmail.com>
2025-03-22 22:49:44 +0900

    꽃길 푸는 중...

---
+++ boj/src/Main_14620.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_14620 {
+	static int N, IN, minPrice=Integer.MAX_VALUE;
+	static int[][] board;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	static int[] flowers;
+	static boolean[][] visited;
+	
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine().trim());
+		IN = N-2;
+		
+		/* 초기화 */
+		board = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		flowers = new int[3];
+		visited = new boolean[N][N];
+		
+		/* 로직 */
+		combi(0, 0, 0, 0);
+		
+		/* 출력 */
+		System.out.print(minPrice);
+	}
+
+	static void combi(int cnt, int start, long innerFlag, int tot) {
+		if(tot >= minPrice) return;
+		if(cnt == 3) {
+			minPrice = Math.min(tot, minPrice);
+			return;
+		}
+		for (int i = start; i < IN*IN; i++) {
+			int r = i/IN+1;
+			int c = i%IN+1;
+			if(check(r, c) || visited[r][c] || ((innerFlag & 1<<i) != 0)) continue;
+			System.out.println("cnt: " + cnt + ", i: " + i);
+			flowers[cnt] = i;
+			visited[r][c] = true;
+			for (int j = 0; j < 4; j++) {
+				int nr = r+dr[j];
+				int nc = c+dc[j];
+				visited[nr][nc] = true;
+				tot += board[nr][nc];
+			}
+			combi(cnt+1, i+1, (innerFlag | 1<<i), tot+board[r][c]);
+			visited[r][c] = false;
+			for (int j = 0; j < 4; j++) {
+				int nr = r+dr[j];
+				int nc = c+dc[j];
+				visited[nr][nc] = false;
+				tot -= board[nr][nc];
+			}
+		}
+	}
+
+	static boolean check(int r, int c) {
+		for (int j = 0; j < 4; j++) {
+			int nr = r+dr[j];
+			int nc = c+dc[j];
+			if(visited[nr][nc]) return true;
+		}
+		return false;
+	}
+
+}
