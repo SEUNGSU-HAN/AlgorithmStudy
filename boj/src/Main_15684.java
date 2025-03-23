214521bf2e0598f0331fee7530eb9947bbbda033
KKaeBu <h2sorginal@gmail.com>
2025-03-23 18:37:32 +0900

    사다리 조작 푸는 중...

---
+++ boj/src/Main_15684.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_15684 {
+	static int N, M, H;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
