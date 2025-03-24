70d2033d8f2c1dbac1cc92f1f76cb76d2ee5a6e7
h2sorginal <h2sorginal@gmail.com>
2025-03-24 17:55:14 +0900

    소수 구하기 푸는 중...

---
+++ boj/src/Main_1929.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_1929 {
+	static int M, N;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		/* 로직 */
+		for (int i = M; i < (int)Math.sqrt(N); i++) {
+			int n = i;
+			for (int j = 1; n < N; j++) {
+				
+			}
+		}
+	}
+
+}
