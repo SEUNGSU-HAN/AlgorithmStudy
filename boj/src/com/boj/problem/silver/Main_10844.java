499d393ef6107bac71c350d889a871f5c9573cd3
h2sorginal <h2sorginal@gmail.com>
2025-04-03 16:04:18 +0900

    쉬운 계단 수 clear (DP)

--- boj/src/com/boj/problem/silver/Main_10844.java
+++ boj/src/com/boj/problem/silver/Main_10844.java
@@ -12,18 +12,13 @@ public class Main_10844 {
 		N = Integer.parseInt(br.readLine().trim());
 		
 		/* 초기화 */
-		dp = new long[101][10];
-		dp[1][0] = 0;
-		for (int i = 1; i < 10; i++) {
+		dp = new long[N+1][10];
+		for (int i = 0; i < 10; i++) {
 			dp[1][i] = 1;
-		}
-		dp[2][0] = dp[2][9] = 1;
-		for (int i = 1; i < 9; i++) {
-			dp[2][i] = 2;
 		}
 		
 		/* 로직 */
-		for (int i = 3; i <= 100; i++) {
+		for (int i = 2; i <= N; i++) {
 			for (int j = 0; j < 10; j++) {
 				if(j == 0) dp[i][j] = dp[i-1][1];
 				else if(j == 9) dp[i][j] = dp[i-1][8];
