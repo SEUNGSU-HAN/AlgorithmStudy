6fb70682ee4945da405f33bfa5e0aa586de293ed
h2sorginal <h2sorginal@gmail.com>
2025-04-04 14:12:32 +0900

    알약 푸는 중

---
+++ boj/src/Main_4811.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_4811 {
+	static int N;
+	
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		/* 초기화 */
+		
+		/* 로직 */
+		
+		/* 출력 */
+		
+	}
+
+}
