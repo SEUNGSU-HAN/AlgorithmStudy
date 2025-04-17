d997351b67c58248a1e3e3e0886263a329bfa16c
h2sorginal <h2sorginal@gmail.com>
2025-04-17 17:31:24 +0900

    사랑의 카운슬러 clear (subset)

--- swea/src/com/swea/problem/d4/Solution_사랑의_카운슬러.java
+++ swea/src/com/swea/problem/d4/Solution_사랑의_카운슬러.java
@@ -4,43 +4,75 @@ import java.util.*;
 
 public class Solution_사랑의_카운슬러 {
 	static int T, N;
-	static long[] x;
-	static long[] y;
-	static long[][] loveLine;
+	static long result;
+	static class Point {
+		long x, y;
+
+		public Point(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "(" + x + ", " + y + ")";
+		}
+	}
+	static Point[] warm;
+	static boolean[] visited;
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
 		
 		for (int test_case = 1; test_case <= T; test_case++) {
 			/* 입력 */
 			N = Integer.parseInt(br.readLine().trim());
 			
 			/* 초기화 */
-			x = new long[N];
-			y = new long[N];
+			result = Long.MAX_VALUE;
+			warm = new Point[N];
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				x[i] = Integer.parseInt(st.nextToken());
-				y[i] = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				warm[i] = new Point(x, y);
 			}
-			loveLine = new long[N][N];
+			visited = new boolean[N];
 			
 			/* 로직 */
+			loveline(0, 0);
+			sb.append("#").append(test_case).append(" ").append(result).append("\n");
+		}
+		
+		/* 출력 */
+		System.out.print(sb);
+	}
+
+	static void loveline(int cnt, int flag) {
+		if(Integer.bitCount(flag) > N/2) return;
+		if(cnt == N) {
+			if(Integer.bitCount(flag) != N/2) return;
+			
+			long sx=0, sy=0, ex=0, ey=0;
 			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if(i == j) continue;
-					loveLine[i][j] = x[i]*x[j]+y[i]*y[j];
+				if((flag & 1 << i) > 0) {
+					sx += warm[i].x;
+					sy += warm[i].y;
+				}else {
+					ex += warm[i].x;
+					ey += warm[i].y;
 				}
 			}
 			
-			/* 출력 */
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					System.out.printf("% 3d", loveLine[i][j]);
-				}
-				System.out.println();
-			}
-			System.out.println("================");
+			long vx = ex-sx;
+			long vy = ey-sy;
+			long v = vx*vx + vy*vy;
+			result = Math.min(result, v);
+			return;
 		}
+		loveline(cnt+1, flag | 1 << cnt);
+		loveline(cnt+1, flag);
 	}
 }
