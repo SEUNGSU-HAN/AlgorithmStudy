f18199ba627d53817f6aa84f6a6cd2b1c12dfa87
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2025-05-03 01:13:52 +0900

    야미

---
+++ boj/src/com/boj/problem/bronze/Main_2743.java
@@ -0,0 +1,8 @@
+package com.boj.problem.bronze;
+import java.io.*;
+public class Main_2743 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(br.readLine().trim().length());
+	}
+}
