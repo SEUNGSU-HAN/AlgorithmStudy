0420f9aa2a3eb230039bce7a9f99d217556585fa
KKaeBu <h2sorginal@gmail.com>
2025-05-08 00:00:08 +0900

    야미

---
+++ boj/src/Main_2558.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main_2558 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine().trim());
+		int B = Integer.parseInt(br.readLine().trim());
+		System.out.print(A+B);
+	}
+
+}
